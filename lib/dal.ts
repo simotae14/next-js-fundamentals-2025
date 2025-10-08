40b35b99640354d016a21fa08f32e95156c5f1c9
Simone Taeggi <startae14@gmail.com>
2025-10-09 00:07:31 +0200

    Define functions to fetch data (not server actions)

--- lib/dal.ts
+++ lib/dal.ts
@@ -5,6 +5,32 @@ import { cache } from 'react'
 import { issues, users } from '@/db/schema'
 import { mockDelay } from './utils'
 
-export const getUserByEmail = (email: string) => {
-  return true;
+// Current user
+export const getCurrentUser = async () => {
+  // Grab Json web token from the cookies
+  const session = await getSession()
+  if (!session) return null
+
+  try {
+    const result = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session.userId))
+
+    return result[0] || null
+  } catch (error) {
+    console.error('Error getting user by ID:', error)
+    return null
+  }
+}
+
+// Get user by email
+export const getUserByEmail = async (email: string) => {
+  try {
+    const result = await db.select().from(users).where(eq(users.email, email))
+    return result[0] || null
+  } catch (error) {
+    console.error('Error getting user by email:', error)
+    return null
+  }
 }
